cc8ec96c54fee422bdbc3d39bc4996c4a044b748
Sobhan Haggi <39483396+SOB845@users.noreply.github.com>
2020-01-18 15:26:23 +0330

    Update SHARPEN.py

--- SHARPEN.py
+++ SHARPEN.py
@@ -1,7 +1,13 @@
 import hashlib as h
 import sys
 from colorama import Fore, Style, init
-from RandPwd import randSalt	# The function that produces random text to be used for salting hashes
+from RandPwd import randSalt
+
+try:
+	from colorama import *
+except ModuleNotFoundError:
+	print("### pip install colorama ###")
+	raise
 
 # This needs to be added in order to colorama change text colors properly
 init(convert=True)
@@ -21,34 +27,6 @@ theLogo = '''
 
 '''
 
-# These are the available hashing functions which are named trivially
-# They all get the 'inp' input variable as their input
-
-
-def SHA1(input_pass):
-
-	hashed = h.sha1(input_pass.encode())
-	outp = print("\nYour SHA-1 hashed password is: " + hashed.hexdigest())
-
-	return outp
-
-
-def SHA2(input_pass):
-
-	hashed = h.sha256(input_pass.encode())
-	salted_hash = randSalt()
-	hashed.update(hashed + salted_hash)
-	outp = print("\nYour SHA-2 (256 bit) hashed password is: " + hashed.hexdigest())
-
-	return outp
-
-
-def SHA3(input_pass):
-	hashed = h.sha3_256(input_pass.encode())
-	outp = print("\nYour SHA-3 (256 bit) hashed password is: "+ hashed.hexdigest())
-
-	return outp
-
 # Regardless of the user's choice, prints Sharpen at the beginning
 print(Fore.RED + theLogo + Style.RESET_ALL)
 
@@ -64,11 +42,13 @@ if choose == 1:
 
 elif choose == 2:
 	inp = str(input('\nEnter Your Password: '))
-	SHA2(inp)
+	hashed = h.sha1(inp.encode())
+	print("\nYour SHA-1 hashed password is: " + hashed.hexdigest())
 
 elif choose == 3:
 	inp = str(input('\nEnter Your Password: '))
-	SHA3(inp)
+	hashed = h.sha1(inp.encode())
+	print("\nYour SHA-1 hashed password is: " + hashed.hexdigest())
 
 elif choose == 4:
 	print(Fore.GREEN+"\nMy Ethereum Address is: " + Style.RESET_ALL)
