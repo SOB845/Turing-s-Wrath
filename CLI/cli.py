08c5813576ff680a661197312b3aa67d77a59549
Sobhan Haggi <39483396+SOB845@users.noreply.github.com>
2021-08-22 23:21:37 +0430

    Renamed some variables

--- CLI/cli.py
+++ CLI/cli.py
@@ -1,4 +1,4 @@
-import hashlib as h
+import hashlib as hsl
 from colorama import *
 import sys
 from RandPwd import randSalt
@@ -32,28 +32,23 @@ theLogo = '''
 print(Fore.RED + theLogo + Style.RESET_ALL)
 
 # All options
-choose = int(input("Choose Your Option: \n \n* Text to SHA-256 converter (1) \n* Random Password Generator (2) \n* How does it work? (3) \n* Donations (4) \n"))
+choose = int(input("Choose Your Option: \n \n* Text to SHA-256 converter (1) \n* Random Password Generator (2) \n* How does it work? (3) \n"))
 
 
 if choose == 1:
 	inp = str(input('\nEnter Your Password: '))
-	hashed = h.sha256(inp.encode())
+	hashed = hsl.sha256(inp.encode())
 	print("\n>> Your SHA-2 hashed password is: " + hashed.hexdigest())
 
 elif choose == 2:
 	# lg denotes the lengths of password
-	lg = int(input(">> Enter Password length: "))
-	password = randPass(lg)
+	length = int(input(">> Enter Password length: "))
+	password = randPass(length)
 	print(">> Your Random Password is: "+password)
 
 
 elif choose == 3:
 	print(">> 'Turnig's Wrath' is a random password generator and text-to-hash converter. It's extremely easy to use, just type the instruction numbers and stay safe on the internet.")
-
-
-elif choose == 4:
-	print(Fore.WHITE + ">> If you enjoyed Turnig's Wrath, please concider sending us your tips :) \n" + Style.RESET_ALL)
-	print(Fore.GREEN+"\n>> Our Ethereum Address is: 0xaA845d9D5C588Ee6E63D55544faf15466fc7DEA6" + Style.RESET_ALL)
 
 else:
 	raise ValueError(Fore.RED+"Error! Undefined input value!" + Style.RESET_ALL)
