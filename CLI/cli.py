2ee4558a5bfc7536c00ac71e6ad3d167d4b8d5c0
Sobhan Haggi <39483396+SOB845@users.noreply.github.com>
2021-09-11 21:17:51 +0430

    Update cli.py
    Added Exit option & 5 random passwords at a time

--- CLI/cli.py
+++ CLI/cli.py
@@ -31,25 +31,33 @@ theLogo = '''
 # Regardless of the user's choice, prints Sharpen at the beginning
 print(Fore.RED + theLogo + Style.RESET_ALL)
 
-# All options
-choose = int(input("Choose Your Option: \n \n* Text to SHA-256 converter (1) \n* Random Password Generator (2) \n* How does it work? (3) \n"))
+While True:
+	print("\n")
+	# All options
+	choose = int(input("Choose Your Option: \n \n* Text to SHA-256 converter (1) \n* Random Password Generator (2) \n* How does it work? (3) \n* Exit (4) \n"))
+
+	if choose == 1:
+		inp = str(input('\nEnter Your Password: '))
+		hashed = hsl.sha256(inp.encode())
+		print("\n>> Your SHA-2 hashed password is: " + hashed.hexdigest())
+
+	elif choose == 2:
+		length = int(input(">> Enter Password length: "))
+		i = 0
+		while i < length:
+			password = randPass(length)
+			print(">> Your Random Password is: "+password)
+			i+=1
+			if i == 5:
+				break
 
 
-if choose == 1:
-	inp = str(input('\nEnter Your Password: '))
-	hashed = hsl.sha256(inp.encode())
-	print("\n>> Your SHA-2 hashed password is: " + hashed.hexdigest())
-
-elif choose == 2:
-	# lg denotes the lengths of password
-	length = int(input(">> Enter Password length: "))
-	password = randPass(length)
-	print(">> Your Random Password is: "+password)
-
-
-elif choose == 3:
-	print(">> 'Turnig's Wrath' is a random password generator and text-to-hash converter. It's extremely easy to use, just type the instruction numbers and stay safe on the internet.")
-
-else:
-	raise ValueError(Fore.RED+"Error! Undefined input value!" + Style.RESET_ALL)
-	sys.exit()
+	elif choose == 3:
+		print(">> 'Turnig's Wrath' is a random password generator and text-to-hash converter. It's extremely easy to use, just type the instruction numbers and stay safe on the internet.")
+		
+	elif choose = 4:
+		sys.exit()
+		
+	else:
+		raise ValueError(Fore.RED+"Error! Undefined input value!" + Style.RESET_ALL)
+		sys.exit()
