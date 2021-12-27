565061e0eb94b9dc28983cdc0dbb371d8979a9ca
Sobhan Haggi <39483396+SOB845@users.noreply.github.com>
2021-12-27 14:37:30 +0330

    Update gui.py
    Added database and password saving capability

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,17 +1,16 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import messagebox
-from randpass_generator import *
 from pwned import lookup_pwned_api
+from randpass_generator import *
 from string import ascii_letters, digits, punctuation
-
+from Database import save_password
 
 try:
 	from tkinter import *
 except ModuleNotFoundError:
 	print("### pip install tkinter ###")
 	raise
-
 
 # HERE ARE THE ESSENTIAL FUNTIONS (REFERRED AS COMMANDS IN TKINTER) THAT KEEP THE PROGRAM RUNNING.
 # UNLESS YOU KNOW WHAT YOU'RE DOING, PLEASE DON'T TOUCH THEM.
@@ -53,13 +52,31 @@ def CheckPwnageWindow():
 	global checkWin
 	checkWin = Toplevel(root)
 	checkWin.title("Check pwnage")
-	Label(checkWin, text="Enter your password to see if it's been pwned (Requires internet connection)").pack()
-	Entry(checkWin, textvariable=GetTxt, width=30,bd=2).pack()
+	Label(checkWin, text="Enter your password to see if it's been pwned (Requires internet connection)",font=('Ariel',11)).pack()
+	Entry(checkWin, textvariable=GetTxt, width=30,bd=2).pack(pady=10)
 	Button(checkWin, height=2, width= 7, text="SEARCH", command=call_lookup_api).pack(padx=1, pady=10, ipadx=5, ipady=1)
 	Button(checkWin, height=2, width=7, text="BACK", command=Destroy_CheckPwnageWindow).pack(padx=1, pady=10, ipadx=5, ipady=1)
-	checkWin.geometry("440x220")
+	checkWin.geometry("500x220")
 	checkWin.iconbitmap('AlanTuring(64x64).ico')
 
+def save_to_database():
+	d1 = Service_Saver.get()
+	d2 = Password_Saver.get()
+	save_password(d1,d2)
+	return None
+
+def SaveWindow():
+	global savewin
+	savewin = Toplevel(root)
+	Label(savewin, text="Type the password and its associated service\n",font=('Ariel',12)).grid(row=0, column=1)
+	Label(savewin,text="Service/App:").grid(row=1, column=0,ipady=10)
+	Entry(savewin,width=35,bd=2, textvariable=Service_Saver).grid(row=1,column=1)
+	Label(savewin, text="Password:").grid(row=2,column=0)
+	Entry(savewin,width=35,bd=2, textvariable=Password_Saver).grid(row=2,column=1)
+	Button(savewin, text="SAVE",height=2, width=7, command=save_to_database).grid(row=3, column=1,padx=1, pady=18, ipadx=5, ipady=1)
+	savewin.title("Save password")
+	savewin.geometry("440x220")
+	savewin.iconbitmap('AlanTuring(64x64).ico')
 
 def Clr():
 	textfield.config(state="normal")
@@ -88,9 +105,12 @@ sld = Scale(root, from_=1, to=100, length=600, orient=HORIZONTAL)
 sld.pack()
 
 ### Data storage
-NUMS = BooleanVar()	# Stores Boolean data for exclude_digits checkbox
-SYMBS = BooleanVar() # Stores Boolean data for exclude_symbols checkbox
-GetTxt = StringVar()	# Stores entry data for call_lookup_api function
+NUMS = BooleanVar()	#Stores Boolean data for exclude_digits checkbox
+SYMBS = BooleanVar() #Stores Boolean data for exclude_symbols checkbox
+GetTxt = StringVar() #Stores entry data for call_lookup_api function
+Service_Saver = StringVar()	#Stores entry data for SaveWindow
+Password_Saver = StringVar() #Stores entry data for SaveWindow
+
 
 ### Checkboxes
 exclude_digits= Checkbutton(root, text="EXCLUDE NUMBERS FROM PASSWORD", variable = NUMS)
@@ -103,6 +123,8 @@ Generator_Button = Button(root, text="GENERATE PASSWORD", height=2, command=Gene
 Generator_Button.pack(padx=20, pady=5, ipadx= 5, ipady = 2)
 CheckPwnage = Button(root, height=2, text="CHECK IF PWNED", command=CheckPwnageWindow)
 CheckPwnage.pack(padx=20, pady=10, ipadx=5, ipady=2)
+Save = Button(root, text="SAVE PASSWORD", height=2, command=SaveWindow)
+Save.pack(padx=20, pady=10, ipadx=5, ipady=2)
 quit = Button(root, text="QUIT", width=6, height=2, command=Quit)
 quit.pack(padx=20, pady=10, ipadx=5, ipady=2)
 
@@ -114,7 +136,7 @@ textfield.pack()
 clrText = Button(root, text="Clear Everything", width=12, height=2, fg="white" ,bg="red", command=Clr)
 clrText.pack(padx= 20, pady=10, ipadx=5, ipady=2)
 
-root.eval('tk::PlaceWindow . center')	# Centers the app window (NOT SO ACCURATELY)
 root.iconbitmap('AlanTuring(64x64).ico')
-root.geometry("700x700")
+root.geometry("700x710")
+root.eval('tk::PlaceWindow . center')	# Centers the app window (NOT SO ACCURATELY)
 root.mainloop()	# Keeps program running
