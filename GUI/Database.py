78f53cb6e94cadb11569289c8e954520a8716ad9
Sobhan Haggi <39483396+SOB845@users.noreply.github.com>
2022-07-06 21:28:52 +0430

    Update Database.py
    Fixed Null values not functioning

--- GUI/Database.py
+++ GUI/Database.py
@@ -1,12 +1,15 @@
 import sqlite3
+from sqlite import *
 import datetime
 
 def save_password(user_service,user_password):
+    global connection
+    global cursor
     current_time = datetime.datetime.utcnow()
     connection = sqlite3.connect('userinfo.db')
     cursor = connection.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS user_data (service text NOT NULL, password text NOT NULL, date_added text NOT NULL)")
-    cursor.execute("INSERT INTO user_data VALUES (?,?,?)",(user_service,user_password,current_time))
+    cursor.execute("CREATE TABLE IF NOT EXISTS user_data (service text NOT NULL, username text, password text NOT NULL, date_added text NOT NULL)")
+    cursor.execute(""INSERT INTO user_data VALUES (?,?,?,?)",(user_service,user_name or None,user_password,current_time"))
     connection.commit()
     connection.close()
     return None
