10809ab68fafa27bc27cd9fab6a87cd75bffc1d6
Sobhan Haggi <39483396+SOB845@users.noreply.github.com>
2024-03-16 16:02:20 +0330

    Temporary Update Database.py

--- GUI/Database.py
+++ GUI/Database.py
@@ -4,8 +4,8 @@ import datetime
 import os
 
 global path
-current_path = os.getcwd() # The path where the program was run for the first time
-database_path = current_path + "\\userinfo.db" # This is the path your database resides in
+current_path = os.getcwd()
+database_path = current_path + "\\userinfo.db"
 
 def create_connection(db_file):
     global conn
@@ -16,16 +16,29 @@ def create_connection(db_file):
     except Error as e:
         print(e)
 
-def save_password(user_service,user_name,user_password):
+def save_or_update_password(input_service,input_name,input_password):
     global connection
     global cursor
 
     create_connection(database_path)
-    current_time = datetime.datetime.utcnow()
+    current_time = str(datetime.datetime.utcnow())
 
     cursor = conn.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS user_data (service text NOT NULL, username text, password text NOT NULL, date_added text NOT NULL)")
-    cursor.execute("INSERT INTO user_data VALUES (?,?,?,?)",(user_service,user_name or None,user_password,current_time))
+    cursor.execute("SELECT * FROM user_data WHERE service = ? AND username = ?", (input_service, input_name or None)) # Check if Service and Username already in database
+    conn.commit()
+    res = cursor.fetchall()
+    print(len(res))
+
+    if res is not None: # There is at least one entry from past that's same with new inputs
+        cursor.execute("UPDATE user_data SET password = ?, username = ?, date_added = ? WHERE service = ?", (input_password, input_name or None, current_time, input_service))
+        print("Data updated")
+    else:
+        cursor.execute("INSERT INTO user_data (service, username, password, date_added) VALUES (?,?,?,?)", (input_service, input_name or None, input_password, current_time))
+        print("Data inserted")
+
     conn.commit()
     conn.close()
-    return None
+
+# Update works fine but does not save new entries
+# LINE 28 COULD BE PROBLEMATIC
